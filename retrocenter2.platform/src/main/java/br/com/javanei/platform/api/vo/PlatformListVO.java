180c7ed07353d154cae53dfc08c58758a6eb75f3
Vanei Anderson Heidemann <vanei.heidemann@gmail.com>
2021-10-13 14:33:25 -0300

    [Platform] Added OpenAPI documentation to resources and objects

---
+++ retrocenter2.platform/src/main/java/br/com/javanei/platform/api/vo/PlatformListVO.java
@@ -0,0 +1,29 @@
+package br.com.javanei.platform.api.vo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Schema(name="PlatformList", description="POJO representing the list of platforms")
+public class PlatformListVO {
+    @Schema(required = true)
+    private List<PlatformVO> platforms = new LinkedList<>();
+
+    public List<PlatformVO> getPlatforms() {
+        return platforms;
+    }
+
+    public PlatformListVO setPlatforms(List<PlatformVO> platforms) {
+        this.platforms = platforms;
+        return this;
+    }
+    
+    public void add(PlatformVO platform) {
+        platforms.add(platform);
+    }
+
+    public int getSize() {
+        return platforms.size();
+    }
+}
